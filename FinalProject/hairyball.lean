2a329a0938b1631cae17ce59233d47659511d355
Robert Trosten <roberttrosten@Roberts-MacBook-Pro-7.local>
2023-12-04 12:51:51 -0500

    trying again

--- FinalProject/hairyball.lean
+++ FinalProject/hairyball.lean
@@ -12,20 +12,30 @@ structure IsEqvSphVF {n : ℕ}
   (v : EuclideanSpace ℝ (Fin n) → EuclideanSpace ℝ (Fin n)) extends IsSphVF v where
   equiv : ∀ r > (0 : ℝ), ∀ x, v (r • x) = r • v x
 
-open Polynomial
+open Polynomial MeasureTheory Metric ENNReal
 
 def IsPolynomialFun (f : ℝ → ℝ) := ∃ P : ℝ[X], f = P.eval
 
 example (P Q : ℝ[X]) (h : P.eval = Q.eval) : P = Q := Polynomial.funext (congrFun h)
 
-lemma sqrt_poly {n} (h : IsPolynomialFun (fun x ↦ (1+x^2)^(n/2))) : Even n :=
 
+
+lemma sqrt_poly {n} (h : IsPolynomialFun (fun x ↦ (1+x^2)^(n/2))) : Even n := by
+
+  let q : ℝ → ℝ := fun x ↦ (1 + x^2)^(n/2)
+  have hq : IsPolynomialFun (q*q) := by
+    use (1+X^2)^n
+    ext z
+    simp
+    sorry
+  -- have h : ∀ (x : ℝ) (p * p - q * q).eval x = 0 := sorry
+  -- have bruh := zero_of_eval_zero p*p-q*q
   sorry
 
 theorem hairy_ball_aux {n} {v : E n → E n} (h : IsEqvSphVF v) (h' : ∀x, ‖x‖ = 1 → v x ≠ 0) : Even n := sorry
 
 theorem hairy_ball {n} {v : E n → E n} (h : IsSphVF v) (h' : ∀x, ‖x‖ = 1 → v x ≠ 0) : Even n := sorry
 
-open MeasureTheory Metric ENNReal
+
 
 #check volume (ball (0 : E 3) 1)

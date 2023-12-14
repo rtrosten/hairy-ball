f04b7a0e911efd0e154df4dba485845259dc662b
Matthew Snodgrass <matthewsnodgrass@matthews-mbp-67.wifi.local.cmu.edu>
2023-12-14 13:50:21 -0500

    convex

--- FinalProject/hairyball.lean
+++ FinalProject/hairyball.lean
@@ -18,14 +18,14 @@ def IsPolynomialFun (f : ℝ → ℝ) := ∃ P : ℝ[X], f = P.eval
 
 example (P Q : ℝ[X]) (h : P.eval = Q.eval) : P = Q := Polynomial.funext (congrFun h)
 
-lemma c1_implies_lipschitz (v : E n → E n) (hv : IsSphVF v) (A : Set (E n)) (hA: convex A) : ∃ K : NNReal, LipschitzWith K (Set.restrict v A) := by
+lemma c1_implies_lipschitz (v : E n → E n) (hv : ContDiff ℝ ⊤ v) (A : Set (E n)) (hA : Convex ℝ A) : ∃ K : NNReal, LipschitzWith K (Set.restrict A v) := by
+
   sorry
 
-lemma sqrt_poly {n} (h : IsPolynomialFun (fun x ↦ (1+x^2)^(n/2))) : Even n := by
-
-  let q : ℝ → ℝ := fun x ↦ (1 + x^2)^(n/2 : ℝ)
-  have hq : IsPolynomialFun (q*q) := by
-    use (1+X^2)^n
+lemma sqrt_poly {n} (h : IsPolynomialFun (fun x ↦ (1 + x^2)^(n / 2))) : Even n := by
+  let q : ℝ → ℝ := fun x ↦ (1 + x^2)^(n / 2 : ℝ)
+  have hq : IsPolynomialFun (q * q) := by
+    use (1 + X^2)^n
     ext z
     simp
     rw [← Real.rpow_add, ← Real.rpow_nat_cast]
@@ -36,6 +36,10 @@ lemma sqrt_poly {n} (h : IsPolynomialFun (fun x ↦ (1+x^2)^(n/2))) : Even n := 
   have h : ∀ (x : ℝ) (p * p - q * q).eval x = 0 := sorry
   have bruh := zero_of_eval_zero p*p-q*q
   sorry
+
+lemma poly_transform (v : E n → E n) (hv : ContDiff ℝ ⊤ v) (A : Set (E n)) (hA : IsCompact A) :
+  ∀ᶠ t in 𝓝 0, (Function.Injective (fun x : A ↦ x + t • (v x)) ∧
+  IsPolynomialFun (fun t ↦ volume ((fun x : A ↦ x + t • (v x))''A))) := sorry
 
 theorem hairy_ball_aux {n} {v : E n → E n} (h : IsEqvSphVF v) (h' : ∀x, ‖x‖ = 1 → v x ≠ 0) : Even n := sorry
 
